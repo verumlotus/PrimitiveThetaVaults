f02fa148e8cb7e91f35a6c62ffb948fa829fce9f
verumlotus <universumtremendum@gmail.com>
2022-01-19 13:19:29 -0800

    Create primitive call back interface utility

--- src/libraries/VaultLifecycle.sol
+++ src/libraries/VaultLifecycle.sol
@@ -1,6 +1,16 @@
-// SPDX-License-Identifier: Unlicense
+// SPDX-License-Identifier: GPL
 pragma solidity 0.8.11;
+import "primitive/interfaces/IPrimitiveEngine.sol";
+import "./ShareMath.sol";
 
 library VaultLifecycle {
-    
+
+    struct openPositionParams {
+        address engine; // Address of the primitive engine for the asset/stable pair
+        uint256 assetAmt; // asset amount to deposit as LP in RMM Pool
+        uint256 stableAmt; // stable amount to deposit as LP in RMM Pool
+
+    }
+
+    function openPosition()
 }
