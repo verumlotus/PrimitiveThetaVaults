c1172b1b70feb83e01c7ed3e88c979ddfb7d55cb
verumlotus <universumtremendum@gmail.com>
2022-01-18 18:34:05 -0800

    Add Vault.sol

--- src/libraries/ShareMath.sol
+++ src/libraries/ShareMath.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.11;
 
-import {Vault} from "./Vault.sol";
+import "./Vault.sol";
 
 library ShareMath {
 
---
+++ src/libraries/Vault.sol
@@ -0,0 +1,75 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.11;
+
+library Vault {
+    /************************************************
+     *  IMMUTABLES & CONSTANTS
+     ***********************************************/
+
+    // Fees are 6-decimal places. For example: 20 * 10**6 = 20%
+    uint256 internal constant FEE_MULTIPLIER = 10**6;
+
+    // Premium discount has 1-decimal place. For example: 80 * 10**1 = 80%. Which represents a 20% discount.
+    uint256 internal constant PREMIUM_DISCOUNT_MULTIPLIER = 10;
+
+    // Percentage of funds allocated to options is 2 decimal places. 10 * 10**2 = 10%
+    uint256 internal constant OPTION_ALLOCATION_MULTIPLIER = 10**2;
+
+    // Placeholder uint value to prevent cold writes
+    uint256 internal constant PLACEHOLDER_UINT = 1;
+
+    struct VaultParams {
+        // Token decimals for vault shares
+        uint8 decimals;
+        // Risky asset of RMM pool
+        address asset;
+        // Riskless (stable) asset of RMM pool
+        address stable;
+        // Minimum supply of the vault shares issued, for ETH it's 10**10
+        uint56 minimumSupply;
+        // Vault cap
+        uint104 cap;
+    }
+
+    struct OptionState {
+        // Option that the vault is shorting / longing in the next cycle
+        address nextOption;
+        // Option that the vault is currently shorting / longing
+        address currentOption;
+        // The timestamp when the `nextOption` can be used by the vault
+        uint32 nextOptionReadyAt;
+    }
+
+    struct VaultState {
+        // 32 byte slot 1
+        //  Current round number. `round` represents the number of `period`s elapsed.
+        uint16 round;
+        // Amount that is currently locked for selling options
+        uint104 lockedAmount;
+        // Amount that was locked for selling options in the previous round
+        // used for calculating performance fee deduction
+        uint104 lastLockedAmount;
+        // 32 byte slot 2
+        // Stores the total tally of how much of `asset` there is
+        // to be used to mint rTHETA tokens
+        uint128 totalPending;
+        // Amount locked for scheduled withdrawals;
+        uint128 queuedWithdrawShares;
+    }
+
+    struct DepositReceipt {
+        // Maximum of 65535 rounds. Assuming 1 round is 7 days, maximum is 1256 years.
+        uint16 round;
+        // Deposit amount, max 20,282,409,603,651 or 20 trillion ETH deposit
+        uint104 amount;
+        // Unredeemed shares balance
+        uint128 unredeemedShares;
+    }
+
+    struct Withdrawal {
+        // Maximum of 65535 rounds. Assuming 1 round is 7 days, maximum is 1256 years.
+        uint16 round;
+        // Number of shares withdrawn
+        uint128 shares;
+    }
+}
