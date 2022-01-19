5212974f62ae2f423b0a8bd6fa166ee8595ae71e
verumlotus <universumtremendum@gmail.com>
2022-01-18 17:59:22 -0800

    Add ShareMath.sol, add check shares.decimal == asset.decimal in vault test

---
+++ src/test/PrimitiveRmm01.t.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity 0.8.11;
+
+import {DSTest} from "ds-test/test.sol";
+import {Utilities} from "./utils/Utilities.sol";
+import {console} from "./utils/Console.sol";
+import {Hevm} from "./utils/Hevm.sol";
+import {Vault} from "../vaults/Vault.sol";
+
+contract PrimitiveRmm01Test is DSTest {
+    Hevm internal immutable hevm = Hevm(HEVM_ADDRESS);
+    Vault internal immutable vault;
+
+    Utilities internal utils;
+    address payable[] internal users;
+
+    function setUp() public {
+        utils = new Utilities();
+        users = utils.createUsers(5);
+        // TODO instantiate vault here
+    }
+
+    function testExample() public {
+        address payable alice = users[0];
+        console.log("alice's address", alice);
+        address payable bob = users[1];
+        hevm.prank(alice);
+        (bool sent, ) = bob.call{value: 10 ether}("");
+        assertTrue(sent);
+        assertGt(bob.balance, alice.balance);
+    }
+
+    function testSharesDecimalEqualsAssetDecimals() public {
+        assertEq(vault.decimals(), vault.underlying().decimals());
+    }
+}
---
+++ src/utils/ShareMath.sol
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.11;
+
+import {Vault} from "./Vault.sol";
+
+library ShareMath {
+
+    uint256 internal constant PLACEHOLDER_UINT = 1;
+
+    function assetToShares(
+        uint256 assetAmount,
+        uint256 assetPerShare,
+        uint256 decimals
+    ) internal pure returns (uint256) {
+        return assetAmount * (10 ** decimals) / assetPerShare;
+    }
+
+    function sharesToAsset(
+        uint256 shares,
+        uint256 assetPerShare,
+        uint256 decimals
+    ) internal pure returns (uint256) {
+        return shares * assetPerShare / decimals;
+    }
+
+    /**
+     * @notice Returns the shares unredeemed by the user given their DepositReceipt
+     * @param depositReceipt is the user's deposit receipt
+     * @param currentRound is the `round` stored on the vault
+     * @param assetPerShare is the price in asset per share
+     * @param decimals is the number of decimals the shares/asset uses
+     * @return unredeemedShares is the user's virtual balance of shares that are owed
+     */
+    function getSharesFromReceipt(
+        Vault.DepositReceipt memory depositReceipt,
+        uint256 currentRound,
+        uint256 assetPerShare,
+        uint256 decimals
+    ) internal pure returns (uint256 unredeemedShares) {
+        if (depositReceipt.round > 0 && depositReceipt.round < currentRound) {
+            uint256 sharesFromRound =
+                assetToShares(depositReceipt.amount, assetPerShare, decimals);
+
+            return
+                uint256(depositReceipt.unredeemedShares) + sharesFromRound;
+        }
+        return depositReceipt.unredeemedShares;
+    }
+
+    // function pricePerShare
+    /** 
+     * @notice Returns the number of assets per unit of share
+     * @param totalSupply is the total supply of shares
+     * @param totalBalance is the total balance of the asset held by this vault
+     * @param pendingAmount is the pending amount of assets that haven't been converted into shares yet
+     * @param decimals is the number of decimals the shares/asset uses
+     */
+    function assetPerShare(
+        uint256 totalSupply,
+        uint256 totalBalance,
+        uint256 pendingAmount,
+        uint256 decimals
+    ) internal pure returns (uint256) {
+        uint256 singleShare = 10**decimals;
+        return
+            totalSupply > 0
+                ? singleShare * (totalBalance - pendingAmount) / totalSupply
+                : singleShare;
+    }
+
+    /************************************************
+     *  HELPERS
+     ***********************************************/
+
+    function assertUint104(uint256 num) internal pure {
+        require(num <= type(uint104).max, "Overflow uint104");
+    }
+
+    function assertUint128(uint256 num) internal pure {
+        require(num <= type(uint128).max, "Overflow uint128");
+    }
+}
