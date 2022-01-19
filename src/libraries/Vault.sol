fb2f158eaf1a11eb9e4eb8c38ea0a6671e0f087d
verumlotus <universumtremendum@gmail.com>
2022-01-19 12:32:52 -0800

    Update Vault.sol w/ better structs and comments

--- src/libraries/Vault.sol
+++ src/libraries/Vault.sol
@@ -9,12 +9,6 @@ library Vault {
     // Fees are 6-decimal places. For example: 20 * 10**6 = 20%
     uint256 internal constant FEE_MULTIPLIER = 10**6;
 
-    // Premium discount has 1-decimal place. For example: 80 * 10**1 = 80%. Which represents a 20% discount.
-    uint256 internal constant PREMIUM_DISCOUNT_MULTIPLIER = 10;
-
-    // Percentage of funds allocated to options is 2 decimal places. 10 * 10**2 = 10%
-    uint256 internal constant OPTION_ALLOCATION_MULTIPLIER = 10**2;
-
     // Placeholder uint value to prevent cold writes
     uint256 internal constant PLACEHOLDER_UINT = 1;
 
@@ -32,35 +26,31 @@ library Vault {
     }
 
     struct OptionState {
-        // Option that the vault is shorting / longing in the next cycle
-        address nextOption;
         // Option that the vault is currently shorting / longing
         address currentOption;
-        // The timestamp when the `nextOption` can be used by the vault
+        // The timestamp when we can rollover to the next option
         uint32 nextOptionReadyAt;
     }
 
     struct VaultState {
-        // 32 byte slot 1
         //  Current round number. `round` represents the number of `period`s elapsed.
         uint16 round;
-        // Amount that is currently locked for selling options
-        uint104 lockedAmount;
-        // Amount that was locked for selling options in the previous round
-        // used for calculating performance fee deduction
-        uint104 lastLockedAmount;
-        // 32 byte slot 2
-        // Stores the total tally of how much of `asset` there is
-        // to be used to mint rTHETA tokens
+        // Amount of total liquidity we have in our RMM Pool
+        uint256 delLiquidityAmount;
+        // Amount in asset that was present at the end of the previous round
+        // used to calculate vault fees
+        uint256 lastRoundAssetAmount;
+        // Stores the total tally of how much of `asset` there is in pending deposits
+        // to be used to mint shares
         uint128 totalPending;
-        // Amount locked for scheduled withdrawals;
+        // Amount of shares queued for scheduled withdrawals;
         uint128 queuedWithdrawShares;
     }
 
     struct DepositReceipt {
         // Maximum of 65535 rounds. Assuming 1 round is 7 days, maximum is 1256 years.
         uint16 round;
-        // Deposit amount, max 20,282,409,603,651 or 20 trillion ETH deposit
+        // Deposit amount in asset
         uint104 amount;
         // Unredeemed shares balance
         uint128 unredeemedShares;
